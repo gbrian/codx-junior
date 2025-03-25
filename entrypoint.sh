b92e9af2c4f299c0b81ef1718ed8e849acbf6489
gbrian <gustavo.brian@gmail.com>
2025-03-25 16:35:53 +0000

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,17 @@ echo "codx-junior entrypoint"
 if [ ! -d "${HOME}/codx-junior" ]; then
   echo "Cloning codx-junior: $CODX_JUNIOR_BRANCH"
   git clone --depth 1 --branch $CODX_JUNIOR_BRANCH https://github.com/gbrian/codx-junior.git ${HOME}/codx-junior
-  cd ${HOME}/codx-junior
-  bash codx-junior install
 fi
 
 cd ${HOME}/codx-junior
+
+if [ ! -f "${HOME}/codx-junior/installed" ]; then
+  echo "Installing codx-junior at ${HOME}/codx-junior"
+  bash codx-junior install
+  touch ${HOME}/codx-junior/installed
+fi
+
 # Run supervisor
 echo "Running supervisor"
-sudo bash ${HOME}/codx-junior/codx-junior supervisor
+mkdir -p $CODX_SUPERVISOR_LOG_FOLDER
+sudo bash -c "USER=$USER HOME=$HOME bash ${HOME}/codx-junior/codx-junior supervisor"
