d45e445695e38497b095e8278590930c90e1205b
gbrian <gustavo.brian@gmail.com>
2025-03-27 19:04:17 +0000

    Import export settings

--- client/src/views/GlobalSettings.vue
+++ client/src/views/GlobalSettings.vue
@@ -1,16 +1,23 @@
 <script setup>
 import AISettings from './AISettings.vue'
 import AgentSettings from '@/components/ai_settings/AgentSettings.vue'
-import ModelSelector from '@/components/ai_settings/ModelSelector.vue';
+import ModelSelector from '@/components/ai_settings/ModelSelector.vue'
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col gap-2 p-4 overflow-auto">
     <div class="text-xl font-medium my-2 flex justify-between">
       Global Settings
-      <div class="flex justify-end gap-2">
+      <div class="flex justify-end gap-2 items-center">
         <button class="btn btn-sm" @click="reloadSettings">Reload</button>
         <button class="btn btn-sm btn-primary" @click="saveSettings">Save</button>
+        <div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-sm m-1"><i class="fa-solid fa-bars"></i></div>
+          <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-50 w-52 p-2 shadow-sm">
+            <li @click="exportSettings"><a>Export</a></li>
+            <li @click="importSettings"><a>Import</a></li>
+          </ul>
+        </div>
       </div>
     </div>
     <div role="tablist" class="tabs tabs-bordered">
@@ -113,15 +120,24 @@ import ModelSelector from '@/components/ai_settings/ModelSelector.vue';
     <div v-if="activeTab === 'Agents'" class="flex flex-col gap-4">
       <AgentSettings />
     </div>
+    <modal v-if="showImport" :close="true" @close="showImport = false">
+      <div class="flex flex-col gap-2">
+        <div class="text-xl">Paste your JSON settings here</div>
+        <textarea v-model="importData" class="textarea textarea-bordered w-full"></textarea>
+        <button class="btn btn-sm btn-primary" @click="submit">Import</button>
+      </div>
+    </modal>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       activeTab: 'Settings',
-      settings: null
+      settings: null,
+      importData: '',
+      showImport: false
     }
   },
   created () {
@@ -138,6 +154,22 @@ export default {
     },
     reloadSettings() {
       this.loadSettings()
+    },
+    exportSettings() {
+      const settingsText = JSON.stringify(this.settings, null, 2)
+      this.$ui.copyTextToClipboard(settingsText)
+    },
+    importSettings() {
+      this.showImport = true
+    },
+    async submit() {
+      try {
+        const imported = JSON.parse(this.importData)
+        this.settings = { ...this.settings, ...imported }
+      } catch (e) {
+        console.error('Import failed', e)
+      }
+      this.showImport = false
     }
   }
 }
