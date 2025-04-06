eff41008753600d82041f75013754fd35b05468e
gbrian <gustavo.brian@gmail.com>
2025-04-06 16:33:33 +0000

    Deploy fixes

--- set_env.sh
+++ set_env.sh
@@ -3,49 +3,27 @@ export CODX_JUNIOR_API_VENV=/tmp/.venv_codx_junior_api
 # Browser virtual env
 export BROWSER_VENV=/tmp/.venv_codx_junior_browser
 # codx-junior API port
-if [ -z "$CODX_JUNIOR_API_PORT" ];then 
-  export CODX_JUNIOR_API_PORT=19980
-fi
+export CODX_JUNIOR_API_PORT=19980
 # codx-junior API URL
-if [ -z "$CODX_JUNIOR_API_URL" ]; then
-  export CODX_JUNIOR_API_URL=http://0.0.0.0:${CODX_JUNIOR_API_PORT}
-fi
+export CODX_JUNIOR_API_URL=http://0.0.0.0:${CODX_JUNIOR_API_PORT}
 # codx-junior web port
-if [ -z "$CODX_JUNIOR_WEB_PORT" ]; then
-  export CODX_JUNIOR_WEB_PORT=19981
-fi
+export CODX_JUNIOR_WEB_PORT=19981
 # codx-junior code-server port
-if [ -z "$CODX_JUNIOR_CODER_PORT" ]; then
-  export CODX_JUNIOR_CODER_PORT=19982
-fi
+export CODX_JUNIOR_CODER_PORT=19982
 # Desktop port
-if [ -z "$CODX_JUNIOR_NOVNC_PORT" ]; then
-  export CODX_JUNIOR_NOVNC_PORT=19983
-fi
+export CODX_JUNIOR_NOVNC_PORT=19983
 # codx-junior API background port
-if [ -z "$CODX_JUNIOR_API_PORT_BACKGROUND" ]; then
-  export CODX_JUNIOR_API_PORT_BACKGROUND=19984
-fi
+export CODX_JUNIOR_API_PORT_BACKGROUND=19984
 # llmfactory server port
-if [ -z "$CODX_JUNIOR_LLMFACTORY_PORT" ]; then
-  export CODX_JUNIOR_LLMFACTORY_PORT=19985
-fi
-if [ -z "$CODX_JUNIOR_LLMFACTORY_URL" ]; then
-  export CODX_JUNIOR_LLMFACTORY_URL=http://0.0.0.0:${CODX_JUNIOR_LLMFACTORY_PORT}
-fi
+export CODX_JUNIOR_LLMFACTORY_PORT=19985
+export CODX_JUNIOR_LLMFACTORY_URL=http://0.0.0.0:${CODX_JUNIOR_LLMFACTORY_PORT}
 # codx-junior preview display
-if [ -z "$CODX_JUNIOR_DISPLAY" ]; then
-  export CODX_JUNIOR_DISPLAY=:55
-fi
+export CODX_JUNIOR_DISPLAY=:55
 # Serving client from API
-if [ -z "$CODX_SUPERVISOR_LOG_FOLDER" ]; then
-  export CODX_SUPERVISOR_LOG_FOLDER=/var/log/codx-junior-supervisor
-fi
+export CODX_SUPERVISOR_LOG_FOLDER=/var/log/codx-junior-supervisor
 
 ### OLLAMA
-if [ -z "$CODX_JUNIOR_EMBEDDINGS_MODEL" ]; then
-  export CODX_JUNIOR_EMBEDDINGS_MODEL=nomic-embed-text
-fi
+export CODX_JUNIOR_EMBEDDINGS_MODEL=nomic-embed-text
 
 # Miscellaneous
 export DEBIAN_FRONTEND=noninteractive
